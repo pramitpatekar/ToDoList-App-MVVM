d2ba6a51033ccbc92a1dc103f459d2f6afa161d8
pramitpatekar <pramitpatekar19@gmail.com>
2024-07-17 18:58:49 +0530

    5

--- ToDoList/Views/ListView.swift
+++ ToDoList/Views/ListView.swift
@@ -12,24 +12,32 @@ struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
     
     var body: some View {
-        List{
-            ForEach(listViewModel.items) { item in
-                ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
-                        }
+        ZStack {
+            if listViewModel.items.isEmpty {
+                NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            } else {
+                List{
+                    ForEach(listViewModel.items) { item in
+                        ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
+                .listStyle(PlainListStyle())
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
+            
         }
         .navigationTitle("ToDo List")
         .navigationBarItems(
             leading: EditButton().font(.headline), 
             trailing: NavigationLink("Add", destination: AddView())
         )
-        .listStyle(PlainListStyle())
     }
     
 }
